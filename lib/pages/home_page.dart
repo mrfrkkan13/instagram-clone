fcb6cce9ba535b49a44ac20454df732f3d6d85dd
ömer faruk kan <102980221+mrfrkkan13@users.noreply.github.com>
2023-03-16 12:12:15 +0300

    ....

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:insta_clone/component/timeline_stories.dart';
 import 'package:insta_clone/components/timeline_appbar.dart';
 
 import '../components/custom_bottom_bar.dart';
+import '../component/timeline_stories.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
