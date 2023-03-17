ec82162267d390cf398013fafb3eec96cb47c442
ömer faruk kan <102980221+mrfrkkan13@users.noreply.github.com>
2023-03-17 11:12:56 +0300

    update project

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/view/home_screen.dart';
 import 'package:provider/provider.dart';
-
 import 'app_init.dart';
 import 'view_models/discovery_view_model.dart';
 import 'view_models/home_page_view_model.dart';
 import 'view_models/timeline_view_model.dart';
+
+
+
+
+
 
 void main() {
   runApp(const MyApp());
