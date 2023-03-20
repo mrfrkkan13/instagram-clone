3fb3d6229b99c00a480daf26451152fbede63328
ömer faruk kan <102980221+mrfrkkan13@users.noreply.github.com>
2023-03-20 11:33:48 +0300

    merge create post and create live video

--- lib/view/createPost/create_post_screen.dart
+++ lib/view/createPost/create_post_screen.dart
@@ -1,11 +1,35 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:instagram_clone/components/create_post_components/create_post_gridview.dart';
+import 'package:instagram_clone/components/profile_components/profile_gridview_tagged.dart';
+
+import '../../components/create_post_components/create_post_appbar.dart';
+import '../../components/create_post_components/create_post_selected_image.dart';
+import '../../components/create_post_components/create_post_special.dart';
 
 class CreatePostScreen extends StatelessWidget {
   const CreatePostScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+
+      body: SafeArea(
+        child: Column(
+
+          children: [
+            CreatePostBar(),
+            CreatePostSelectedImage(),
+            CreatePostSpecial(),
+            CreatePostGrid(),
+
+
+          ],
+        ),
+      )
+
+    );
   }
 }
